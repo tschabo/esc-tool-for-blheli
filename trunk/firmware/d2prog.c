2e7d5a61634510f9df47c8f1b4a22b77fded815f
mulenker@gmail.com <mulenker@gmail.com@af2a6fe6-155e-c949-ed74-f4a9b4020c22>
2012-09-27 18:37:01 +0000

    Updated Firmware added blh file for target multi version 7 repaired parser for blh files

--- trunk/firmware/d2prog.c
+++ trunk/firmware/d2prog.c
@@ -8,20 +8,18 @@
 #include <util/setbaud.h>
 #include <util/delay.h>
 
-void uartPutchar(char c)
+int uartPutchar(char c, FILE * f)
 {
 	loop_until_bit_is_set(UCSR0A, UDRE0); /* Wait until data register empty. */
 	UDR0 = c;
+	return 1;
 }
 
-char uartGetchar(void)
+int uartGetchar(FILE * f)
 {
 	loop_until_bit_is_set(UCSR0A, RXC0); /* Wait until data exists. */
 	return UDR0;
 }
-
-FILE uartOutput = FDEV_SETUP_STREAM(uartPutchar, NULL, _FDEV_SETUP_WRITE);
-FILE uartInput = FDEV_SETUP_STREAM(NULL, uartGetchar, _FDEV_SETUP_READ);
 
 // commands
 #define GET_VERSION		0x01
@@ -596,8 +594,6 @@ int main(void)
 	PORTB |= (1 << PORTB3); // C2CK signal HIGH
 
 	// init serial interface
-	stdout = &uartOutput;
-	stdin  = &uartInput;
 	UBRR0H = UBRRH_VALUE;
 	UBRR0L = UBRRL_VALUE;
 #if USE_2X
@@ -607,6 +603,8 @@ int main(void)
 #endif
 	UCSR0C = _BV(UCSZ01) | _BV(UCSZ00); /* 8-bit data */
 	UCSR0B = _BV(RXEN0) | _BV(TXEN0);   /* Enable RX and TX */
+
+	fdevopen(&uartPutchar, &uartGetchar);
 
 	char in;
 	while(1)
